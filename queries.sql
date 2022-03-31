ac0b32cc89ab90972eca944ecf8878030f8bddf7
Sarah Gallagher <sarahrosegallagher@gmail.com>
2022-03-31 09:59:39 -0700

    retirement info sql and csv

---
+++ queries.sql
@@ -0,0 +1,42 @@
+--total
+SELECT first_name, last_name 
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1955-12-31';
+
+--1952
+SELECT first_name, last_name 
+FROM employees
+WHERE birth_date BETWEEN '1952-01-01' AND '1952-12-31';
+
+--1953
+SELECT first_name, last_name 
+FROM employees
+WHERE birth_date BETWEEN '1953-01-01' AND '1953-12-31';
+
+--1954
+SELECT first_name, last_name 
+FROM employees
+WHERE birth_date BETWEEN '1954-01-01' AND '1954-12-31'
+
+--1955
+SELECT first_name, last_name 
+FROM employees
+WHERE birth_date BETWEEN '1955-01-01' AND '1955-12-31'
+
+
+--retirement eligibility 
+SELECT first_name, last_name 
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+SELECT * FROM retirement_info
+
+--COUNT retirement eligibility 
+SELECT COUNT(first_name)
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+	AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+
+
--- schema.sql
+++ schema.sql
@@ -37,20 +37,20 @@ CREATE TABLE salaries(
 	PRIMARY KEY (emp_no)
 );
 
-
+DROP TABLE titles
 CREATE TABLE titles(
 	emp_no INT NOT NULL,
 	title VARCHAR NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	PRIMARY KEY (emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
 );
 
+DROP TABLE dept_emp;
 
 CREATE TABLE dept_emp(
-	dept_no VARCHAR(4) NOT NULL,
 	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
 	from_date DATE NOT NULL,
 	to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
